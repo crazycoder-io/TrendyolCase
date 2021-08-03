82db1dd1c77e31c6d9ac7b635a9fb9cfde820f61
Mesut <kilincaslanmesut@gmail.com>
2021-08-03 14:11:20 +0300

    add mongodb and dotenv

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
+"use strict";
+
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const addRequestId = require('express-request-id')({setHeader: false});
+const morgan = require('morgan');
+require("dotenv").config(); // ENV config
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(addRequestId)
+
+
+morgan.token('id', (req) => req.id.split('-')[0])
+
+app.use(morgan(
+  "[:date[iso] #:id] Started :method :url for :remote-addr",
+  {immediate: true}));
+
+app.use(morgan("[:date[iso] #:id] Completed :status :res[content-length] in :response-time ms:"));
+
+const metricsRouter = require("./routes/metrics");
 
 // Body parsing Middleware
 app.use(cors());
@@ -11,17 +29,21 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// MongoDB connection
+require("./config/db")();
+
+app.use("/metrics", metricsRouter);
 app.get(
-    "/",
-    async (req, res) => {
-        return res.status(200).send({
-            message: "Hello Test app",
-        });
-    }
+	"/",
+	async (req, res) => {
+		return res.status(200).send({
+			message: "Hello Test app",
+		});
+	}
 );
 
 app.listen(PORT, () => {
-    console.log(`Connected successfully on port ${PORT}`);
+	console.log(`Connected successfully on port ${PORT}`);
 });
 
 module.exports = app;
