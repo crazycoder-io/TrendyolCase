85fb7aa0759682193d4d0d1b9a239a87dcbe698b
Mesut <kilincaslanmesut@gmail.com>
2021-08-05 10:20:23 +0300

    add dummy data

--- api/utils/sampleData.js
+++ api/utils/sampleData.js
@@ -7,61 +7,61 @@ module.exports = {
         resources: [
           {
             name: "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,300&display=swap",
-            initiatorType: link,
+            initiatorType: "link",
             responseEnd: 13.056,
             transferSize: 1176
           },
           {
             name: "https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap",
-            initiatorType: link,
+            initiatorType: "link",
             responseEnd: 13.128,
             transferSize: 0
           },
           {
             name: "https://perf-analytics-client.herokuapp.com/client/css/styles.css",
-            initiatorType: link,
+            initiatorType: "link",
             responseEnd: 13.104,
             transferSize: 1862
           },
           {
             name: "https://perf-analytics-client.herokuapp.com/client/js/perfAnalytics.min.js",
-            initiatorType: script,
+            initiatorType: "script",
             responseEnd: 13.429,
             transferSize: 1719
           },
           {
             name: "https://blog.crazycoder.io/images/portfolio-pp.jpg",
-            initiatorType: img,
+            initiatorType: "img",
             responseEnd: 14.387,
             transferSize: 0
           },
           {
             name: "https://www.youtube.com/embed/soL62aIlqoM",
-            initiatorType: iframe,
+            initiatorType: "iframe",
             responseEnd: 13.191,
             transferSize: 0
           },
           {
             name: "https://perf-analytics-client.herokuapp.com/favicon.ico",
-            initiatorType: other,
+            initiatorType: "other",
             responseEnd: 14.258,
             transferSize: 0
           },
           {
             name: "https://fonts.gstatic.com/s/rubik/v14/iJWbBXyIfDnIV7nEt3KSJbVDV49rz8sDE3U3f4I.woff2",
-            initiatorType: other,
+            initiatorType: "other",
             responseEnd: 13.926,
             transferSize: 0
           },
           {
             name: "https://fonts.gstatic.com/s/karla/v15/qkBIXvYC6trAT55ZBi1ueQVIjQTDppqaE0lK.woff2",
-            initiatorType: other,
+            initiatorType: "other",
             responseEnd: 13.5,
             transferSize: 12097
           },
           {
             name: "https://fonts.gstatic.com/s/karla/v15/qkBIXvYC6trAT55ZBi1ueQVIjQTDppqaHUlKd7c.woff2",
-            initiatorType: other,
+            initiatorType: "other",
             responseEnd: 13.48,
             transferSize: 5684
           }
