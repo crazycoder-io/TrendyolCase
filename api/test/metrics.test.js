6a3e6010018800bf9ba6aad6434ad868510f7278
Mesut <kilincaslanmesut@gmail.com>
2021-08-05 10:34:00 +0300

    update api test suit

--- api/test/metrics.test.js
+++ api/test/metrics.test.js
@@ -3,24 +3,37 @@ const mongoose = require("mongoose");
 const createServer = require("../server");
 const { SAMPLE_DATA_1 } = require("../utils/sampleData");
 
-beforeAll(async (done) => {
-    const URL = process.env.TEST_DB_STRING || "mongodb://localhost:27017/perfanalytics_test";
+jest.setTimeout(3 * 60 * 1000);
 
-    await mongoose.connect(URL,
-        {useNewUrlParser: true, useUnifiedTopology: true},
-    );
-});
-
-afterAll(async (done) => {
-    await mongoose.connection.db.dropDatabase();
-    await mongoose.connection.close();
-
-    done();
-});
-
-const app = createServer();
+let app;
 
 describe("POST /metrics", function() {
+
+    beforeAll(async (done) => {
+        app = createServer();
+        const URL = process.env.TEST_DB_STRING || "mongodb://localhost:27017/perfanalytics_test";
+
+        try {
+            await mongoose.connect(URL,
+                {useNewUrlParser: true, useUnifiedTopology: true},
+            );
+
+            return;
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    afterAll(async (done) => {
+        try {
+            await mongoose.connection.db.dropDatabase();
+            await mongoose.connection.close();
+        } catch (error) {
+            console.log(error);
+        }
+
+        done();
+    });
 
     it("/collect-metrics response should be OK", (done) => {
         request(app)
