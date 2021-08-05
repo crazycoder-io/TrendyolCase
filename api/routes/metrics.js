5fa1394ab3d97d679a0ccd72f938c645e808a396
Mesut <kilincaslanmesut@gmail.com>
2021-08-05 10:22:29 +0300

    metric route has been written with tdd approach

--- api/routes/metrics.js
+++ api/routes/metrics.js
@@ -14,11 +14,20 @@ router
 
             res.json({ data: [] });
         } catch (error) {
+            console.log(error);
             res.json({ error: { error_message: error.message } });
         }
 	})
-	.post("/collect-metrics", (req, res) => {
-        res.json({ message: "OK!" });
+	.post("/collect-metrics", async (req, res) => {
+        const data = req.body;
+
+        try {
+            await MetricModel.create(data);
+            res.json({ message: "OK!" });
+        } catch (error) {
+            console.log(error);
+            res.json({ error: { error_message: error.message } });
+        }
 	});
 
 
