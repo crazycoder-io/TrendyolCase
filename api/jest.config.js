c9f33bd0aef9f8d7435978bce5a5e5a445a3d8f2
Mesut <kilincaslanmesut@gmail.com>
2021-08-05 22:39:16 +0300

    add jest config file

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,12 +1,10 @@
 const config = {
     verbose: true,
-  };
-  
-  module.exports = config;
-  
-  // Or async function
-  module.exports = async () => {
-    return {
-      verbose: true,
-    };
-  };
+};
+
+module.exports = config;
+
+// Or async function
+module.exports = async () => ({
+    verbose: true,
+});
