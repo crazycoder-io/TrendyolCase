1b29126dd5149952950384ebd58525ac898ea761
Mesut <kilincaslanmesut@gmail.com>
2021-08-08 18:18:39 +0300

    dashboard | Update indent on index page

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -2,16 +2,16 @@ import styles from '../styles/Home.module.css';
 import { Header, Title, Charts, Footer } from '../components';
 
 export default function Home(): JSX.Element {
-  return (
-    <div className={styles.container}>
-      <Header />
+    return (
+        <div className={styles.container}>
+            <Header />
 
-      <main className={styles.main}>
-        <Title />
-        <Charts />
-      </main>
+            <main className={styles.main}>
+                <Title />
+                <Charts />
+            </main>
 
-      <Footer />
-    </div>
-  )
-}
+            <Footer />
+        </div>
+    );
+};
