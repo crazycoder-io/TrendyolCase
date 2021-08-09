3cf05a300a92c9b8d1c8a7c1741698a5c4360612
Mesut <kilincaslanmesut@gmail.com>
2021-08-09 12:09:51 +0300

    dashboard | Update index page with servce and types

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -1,17 +1,43 @@
-import styles from '../styles/Home.module.css';
-import { Header, Title, Charts, Footer } from '../components';
+import { HomeProps } from "../types";
+import { Header, Title, Charts, Footer } from "../components";
+import { SplitChartData } from "../utils";
+import serviceCall from "../service/serviceCall";
+import styles from "../styles/Home.module.css";
 
-export default function Home(): JSX.Element {
+const Home = (props: HomeProps): JSX.Element => {
+    const { chartsData } = props;
     return (
         <div className={styles.container}>
             <Header />
 
             <main className={styles.main}>
                 <Title />
-                <Charts />
+                <Charts chartsData={chartsData}/>
             </main>
 
             <Footer />
         </div>
     );
 };
+
+export async function getStaticProps() {
+    // Call an external API endpoint to get data.
+    const data = {
+        startDate: "2021-08-08T19:42:30.681Z",
+        endDate: "2021-08-08T23:42:30.681Z"
+    };
+    const res = await serviceCall();
+
+    const SplittedData = SplitChartData(res.data.report);
+  
+    // By returning { props: { metrics } }, the Charts component
+    // will receive `metrics` as a prop at build time
+    return {
+        props: {
+            metrics: res.data.report,
+            chartsData: SplittedData,
+        },
+    };
+}
+
+export default Home;
