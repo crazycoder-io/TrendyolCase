e53431eaa98ed529f3f26b3c1b569611d734d472
Mesut <kilincaslanmesut@gmail.com>
2021-08-10 15:30:54 +0300

    dashboard | Finish index page | Finish dashboard

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -1,18 +1,47 @@
-import { HomeProps } from "../types";
-import { Header, Title, Charts, Footer } from "../components";
+import { HomeProps, Metrics, ChartsData } from "../types";
+import { Header, Title, DatePicker, Charts, Footer } from "../components";
+import { LoadingHook, ChartHook, DateHook } from "../hooks";
 import { SplitChartData } from "../utils";
 import serviceCall from "../service/serviceCall";
 import styles from "../styles/Home.module.css";
 
 const Home = (props: HomeProps): JSX.Element => {
-    const { chartsData } = props;
+
+    // Get Hooks
+    const { loading, updateLoading } = LoadingHook();
+    const { chartsData, updateChartsData } = ChartHook(props.chartsData);
+    const { specificDate, updateDate } = DateHook();
+
+    const UpdateCharts = async () => {
+        updateLoading();
+        try {
+            const res = await serviceCall({
+                startDate: specificDate.startDate.toISOString(),
+                endDate: specificDate.endDate.toISOString()
+            });
+
+            const splitData = SplitChartData(res.data.report);
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            updateChartsData(splitData!);
+            updateLoading();
+        } catch (error) {
+            console.log(error);
+            updateLoading();
+        }
+    };
+
     return (
         <div className={styles.container}>
             <Header />
 
             <main className={styles.main}>
                 <Title />
-                <Charts chartsData={chartsData}/>
+                <div className={styles.datePicker}>
+                    <DatePicker specificDate={specificDate} setDate={updateDate} keyword={"startDate"} />
+                    <DatePicker specificDate={specificDate} setDate={updateDate} keyword={"endDate"} />
+                    <button onClick={UpdateCharts} className={styles.dateButton}>Update Charts</button>
+                </div>
+                <Charts chartsData={chartsData} loading={loading} />
             </main>
 
             <Footer />
@@ -20,22 +49,23 @@ const Home = (props: HomeProps): JSX.Element => {
     );
 };
 
-export async function getStaticProps() {
-    // Call an external API endpoint to get data.
-    const data = {
-        startDate: "2021-08-08T19:42:30.681Z",
-        endDate: "2021-08-08T23:42:30.681Z"
+export async function getStaticProps(): Promise<{
+    props: {
+        metrics: Metrics;
+        chartsData: ChartsData;
     };
+}> {
+    // Call an external API endpoint to get data.
     const res = await serviceCall();
 
-    const SplittedData = SplitChartData(res.data.report);
+    const splitData = res.hasOwnProperty("data") && SplitChartData(res.data.report);
   
     // By returning { props: { metrics } }, the Charts component
     // will receive `metrics` as a prop at build time
     return {
         props: {
             metrics: res.data.report,
-            chartsData: SplittedData,
+            chartsData: splitData,
         },
     };
 }
